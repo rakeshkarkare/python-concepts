1f8c904c87bac0834983bf476db4628c137c95ea
rakeshkarkare <rakeshkarkare@gmail.com>
2021-09-11 11:35:33 +0400

    commiting changes for string methods

---
+++ primitive_types/string_methods.py
@@ -0,0 +1,11 @@
+course = "  Python Programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.rstrip())
+print(course.find("pro"))
+print(course.rstrip())
+print(course.replace("Programming", "Automation"))
+print("Pro" in course)
+print("swift" not in course)
