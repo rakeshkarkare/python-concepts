7180628c9ad5b9a4b817c5504630408a08af23b8
rakeshkarkare <rakeshkarkare@gmail.com>
2021-09-11 12:49:20 +0400

    commiting changes for type conversion

--- primitive_types/numbers_example.py
+++ primitive_types/numbers_example.py
@@ -1,3 +1,5 @@
+import math
+
 print(10 + 3)
 print(10 - 3)
 print(10 * 3)
@@ -5,3 +7,8 @@ print(10 / 3)
 print(10 // 3)
 print(10 % 3)
 print(10 ** 3)
+
+print(round(2.9))
+print(abs(-2.9))
+print(math.ceil(2.2))
+print(math.floor(2.2))
