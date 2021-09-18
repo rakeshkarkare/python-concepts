118868838ef80f0a51695d0c1743d3ad9e413952
rakeshkarkare <rakeshkarkare@gmail.com>
2021-09-18 10:37:49 +0400

    commiting changes for class variables examples

---
+++ oops_concepts/class_variables_example.py
@@ -0,0 +1,34 @@
+class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+print(Employee.num_of_emps)
+
+emp_1 = Employee('Test', 'Automation', 50000)
+emp_2 = Employee('Test', 'Python', 60000)
+
+emp_1.raise_amount = 1.05
+
+print(emp_1.__dict__)
+print(Employee.__dict__)
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+print(Employee.num_of_emps)
