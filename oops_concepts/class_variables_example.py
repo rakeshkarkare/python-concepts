26c548554f9863697ca80e301356b47857e57644
rakeshkarkare <rakeshkarkare@gmail.com>
2021-09-19 14:31:06 +0400

    examples for class methods and static methods

--- oops_concepts/class_variables_example.py
+++ oops_concepts/class_variables_example.py
@@ -17,13 +17,29 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday == 6:
+            return False
+        return True
+
 
 print(Employee.num_of_emps)
 
 emp_1 = Employee('Test', 'Automation', 50000)
 emp_2 = Employee('Test', 'Python', 60000)
 
-emp_1.raise_amount = 1.05
+# emp_1.raise_amount = 1.05
+Employee.set_raise_amt(1.05)
 
 print(emp_1.__dict__)
 print(Employee.__dict__)
@@ -32,3 +48,18 @@ print(emp_1.raise_amount)
 print(emp_2.raise_amount)
 
 print(Employee.num_of_emps)
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'John-Smith-10000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
+
+import datetime
+my_date = datetime.date(2021, 7, 11)
+
+print(Employee.is_workday(my_date))
+
