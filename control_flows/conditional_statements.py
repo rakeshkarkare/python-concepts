0dea300239810753f7c7f76f9e9688e71dc68ee3
rakeshkarkare <rakeshkarkare@gmail.com>
2021-10-30 21:27:42 +0400

    commiting changes for comparison operators

---
+++ control_flows/conditional_statements.py
@@ -0,0 +1,9 @@
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink Water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done!")
