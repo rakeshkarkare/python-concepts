528d63c6458a0d4d1fd152aef328ead4cd568ccf
rakeshkarkare <rakeshkarkare@gmail.com>
2021-11-02 21:07:00 +0400

    commiting changes for list examples

---
+++ data_structures/list_examples.py
@@ -0,0 +1,11 @@
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(20))
+print(f"Numbes is {numbers} ")
+chars = list("Hello World")
+print(f"Chars is {chars} ")
+print(len(chars))
+print(combined)
+print(f"Combined is {combined} ")
