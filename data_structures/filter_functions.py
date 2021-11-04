a56679b87bedb1ed6bd03d7575ea9eb637fa4620
rakeshkarkare <rakeshkarkare@gmail.com>
2021-11-04 21:35:15 +0400

    commiting changes for filtered list using filter functions

---
+++ data_structures/filter_functions.py
@@ -0,0 +1,9 @@
+# list of tuples for sorting
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 12),
+]
+
+filtered = list(filter(lambda item: item[1] >= 10, items))
+print(filtered)
