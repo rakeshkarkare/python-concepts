abe771853f0d78226f08b16eb25b6b5d4967a674
rakeshkarkare <rakeshkarkare@gmail.com>
2021-11-05 23:17:56 +0400

    commiting changes for class vs instance methods in python

---
+++ classes_in_python/class_vs_instance_methods.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def zero(cls):
+        return cls(0, 0)
+
+    def draw(self):
+        print(f"Point ({self.x}, {self.y})")
+
+
+point = Point(1, 2)
+point = Point.zero()
+point.draw()
