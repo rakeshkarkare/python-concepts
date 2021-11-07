62e4586a4babc22f1b0200013c57e79ca30575f1
rakeshkarkare <rakeshkarkare@gmail.com>
2021-11-07 22:14:35 +0400

    commiting changes for python inheritance

---
+++ classes_in_python/python_inheritance.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+# Animal : Parent, Base
+# Mammal : Child, sub
+
+
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+m = Mammal()
+m.eat()
+print(m.age)
