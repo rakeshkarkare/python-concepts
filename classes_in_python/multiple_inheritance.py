5a9b76dc5f356e9710757dacd4b3116f90a7d056
rakeshkarkare <rakeshkarkare@gmail.com>
2021-11-07 22:40:47 +0400

    commiting changes for good examplee of inheritance

---
+++ classes_in_python/multiple_inheritance.py
@@ -0,0 +1,16 @@
+class Employee:
+    def greet(self):
+        print("Employee Greet")
+
+
+class Person:
+    def greet(self):
+        print("Person Greet")
+
+
+class Manager(Employee, Person):
+    pass
+
+
+manager = Manager()
+manager.greet()
