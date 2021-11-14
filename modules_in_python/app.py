ae89582f419bc3cf840ab5030dcbb76939b6c4b7
rakeshkarkare <rakeshkarkare@gmail.com>
2021-11-14 22:48:56 +0400

    commiting changes for modules package

--- modules_in_python/app.py
+++ modules_in_python/app.py
@@ -1,7 +1,13 @@
-from ecommerce.shopping.sales import calc_shipping, calc_tax
+# from ecommerce.shopping.sales import calc_shipping, calc_tax
+# from ecommerce.shopping import sales
+
+# sales.calc_shipping()
+
+# calc_shipping()
+# calc_tax()
+
 from ecommerce.shopping import sales
-
-sales.calc_shipping()
-
-calc_shipping()
-calc_tax()
+# print(dir(sales))
+print(sales.__name__)
+print(sales.__package__)
+print(sales.__file__)
--- modules_in_python/ecommerce/shopping/sales.py
+++ modules_in_python/ecommerce/shopping/sales.py
@@ -1,7 +1,8 @@
-from ecommerce.customer import contact  # absolutee import
-from ..customer import contact  # relative import
+# from ecommerce.customer import contact  # absolutee import
+# from ..customer import contact  # relative import
 
-contact.contact_customer()
+# contact.contact_customer()
+print("Sales Initialized", __name__)
 
 
 def calc_tax():
@@ -10,3 +11,8 @@ def calc_tax():
 
 def calc_shipping():
     pass
+
+
+if __name__ == "__main__":
+    print("sales started")
+    calc_tax()
