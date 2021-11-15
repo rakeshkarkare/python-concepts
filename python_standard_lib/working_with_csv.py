a3c2753a80693ac00e28a4ab9002b7c8a251a278
rakeshkarkare <rakeshkarkare@gmail.com>
2021-11-15 09:38:33 +0400

    csv file example

---
+++ python_standard_lib/working_with_csv.py
@@ -0,0 +1,12 @@
+import csv
+with open("data.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerow(["transaction_id", "product_id", "price"])
+    writer.writerow([1000, 1, 5])
+    writer.writerow([1001, 2, 15])
+
+with open("data.csv") as file:
+    reader = csv.reader(file)
+    # print(list(reader))
+    for row in reader:
+        print(row)
